a9989ad458a43938c59a979a4c18d3f72660c7a7
fhersho <fhersho@gmail.com>
2016-08-22 17:45:57 -0500

    refactor

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,13 @@
 'use strict'
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp')
+var sass = require('gulp-sass')
 
-gulp.task('styles',function (){
-    return gulp
-      .src('src/client/scss/app.scss')
-      .pipe(sass().on('error',function(error){
-        return notify().write(err);
-      }))
-      .pipe(gulp.dest('public/css'));
-});
+gulp.task('styles', () => {
+  return gulp
+   .src('src/client/scss/app.scss')
+   .pipe(sass())
+   .pipe(gulp.dest('public/css'))
+})
 
-gulp.task('default', ['styles']);
+gulp.task('default', ['styles'])

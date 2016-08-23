b9f10da38ded3e10940c7ec42734747712b35d9f
Ing. Fernando Padilla velez <Ing. Fernando Padilla velez>
2016-08-22 21:43:04 -0500

    sigo con el curso

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,10 @@
 'use strict'
 
-var gulp = require('gulp')
-var sass = require('gulp-sass')
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const babel = require('babelify')
+const browserify = require('browserify')
+const source = require('vinyl-source-stream')
 
 gulp.task('styles', () => {
   return gulp
@@ -10,4 +13,24 @@ gulp.task('styles', () => {
    .pipe(gulp.dest('public/css'))
 })
 
-gulp.task('default', ['styles'])
+gulp.task('images', () => {
+  return gulp
+    .src('src/client/images/*')
+    .pipe(gulp.dest('public/images'))
+})
+
+gulp.task('web', () => {
+  return gulp
+    .src('src/client/web/*')
+    .pipe(gulp.dest('public'))
+})
+
+gulp.task('scripts', () => {
+  browserify('src/client/js/app.js')
+    .transform(babel)
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('public/js'))
+})
+
+gulp.task('default', ['styles', 'web', 'images', 'scripts'])
